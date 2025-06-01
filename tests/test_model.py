f0b8c5df09baa3eb39b93619638195e308bcb5c2
l00187785 <l00187785@atu.ie>
2025-06-02 05:21:42 +0530

    Update test_model.py

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,11 +1,11 @@
 import pytest
-from app.predictor import predict_sarcasm
+
+from app.predictor import predict_sarcasm, model, vectorizer
 
 def test_model_prediction():
-    result = predict_sarcasm("Oh wonderful, more traffic")
+    result = predict_sarcasm("Oh wonderful, more traffic", model, vectorizer)
     assert result in ["That's so sarcastic of you!", "Sounds genuine."]
 
 def test_model_empty_input():
     with pytest.raises(ValueError):
-        predict_sarcasm("")
-
+        predict_sarcasm("", model, vectorizer)
